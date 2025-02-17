d8582157a7cf44474734c3cedc143a0180342de5
Corbyn <144940013+Blueracey@users.noreply.github.com>
2025-02-17 16:37:40 -0700

    File that turns an image into a text file
    and started an actual changing pixels test

---
+++ output_to_textfile.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+imagepath = "goop.png"
+
+# Loads image
+img = cv2.imread(imagepath)
+assert img is not None, "File is missing"
+
+# Open output file
+with open("outputFile", "w") as out:
+    for row in img: #row is the actual rows in the image 
+        for pixel in row: #pixel is each individual pixel 
+            out.write(str(pixel) + "\n")
+            
+
+
+
+#https://github.com/skxmxm/encrypt_in_image 
+#https://www.geeksforgeeks.org/image-steganography-using-opencv-in-python/ 
+# https://dev.to/erikwhiting88/let-s-hide-a-secret-message-in-an-image-with-python-and-opencv-1jf5 
