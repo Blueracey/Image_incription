54175ad487f328b5c3f2d0f70bf78dbd45c6afdd
Corbyn <144940013+Blueracey@users.noreply.github.com>
2025-03-02 19:15:22 -0700

    Decription now works
    added a function that takes a edited image and pulls out the binary
    
    added a function that turns the binary from the other function into words again

--- Editing_test_2.0.py
+++ Editing_test_2.0.py
@@ -35,9 +35,9 @@ cypher =  { "a":[0,1,1,0,0,0,0,1]
            ," " :[0,0,1,0,0,0,0,0]
              }
 
-pattern = {"red" : 20 , "green" : 35 , "blue" :12}
+pattern = {"red" : 20 , "green" : 35 , "blue" :12, "length" : 120}
 
-imagepath = "bigTest.jpg"
+imagepath = "goop.png"
 
 def isEven(int):
     if (int%2)==0:
@@ -57,11 +57,11 @@ def getMessage():
      for i in split: #seperates all the character and puts each word in it's own list within the main list
           finalletters.append(list(i.lower()))
      
-     final = []
+     final = [] 
      for i in finalletters:
-          listtest = []
+
           
-          final.extend(cypher[" "])
+          final.extend(cypher[" "]) #adds a space 
           for j in i :
                final.extend(cypher[j]) #get's the number that represents each letter 
      
@@ -78,13 +78,22 @@ def get_image(imagepath):
     assert img is not None, "File is missing"
     return img
 
+
+
+
+
+ 
 def encript(img):
+     # takes the image and interates through it following  the pattern provided to it 
+     # it then changes values by 1 in order to makie them positive or negative so we can later read taht as binary 
+     # it then saves the image and exits
+
         red = pattern["red"]
         green = pattern["green"]
         blue = pattern["blue"]
-        message = getMessage()
+        message = getMessage() #get message returns a list of binary  values to put into the image
         finalImg = img
-        print(message)
+
         redCount = 1
         blueCount = 0
         greenCount = 0
@@ -102,28 +111,28 @@ def encript(img):
                               
 
                               if message[wordcount] == 0 : #if the pixel value will be even
-                                   print("0")
+                                   
                                    if isEven(pixel[0]) : #detects that pixel is even meaning  no changes need to be made 
-                                        print("even 0")
+                                        
                                         #access the red value
                                         wordcount = wordcount+1
                                    else : #detects pixel is odd so it changes  it to even to represent 0 
-                                        print("odd 0")
+                                        
                                         if pixel[0] == 255: # checks to make sure the number is not 255 which is the max value of an RGB value
                                              pixel[0] = pixel[0]-1 #just makes it -1 to make it even instead of odd.    
                                         else:
                                              pixel[0] = pixel[0]+1
                                         wordcount = wordcount+1
                               else : #if the pixel value will be odd
-                                   print("1")
+                                   
                                    if isEven(pixel[0]) :   #checks if the pixel is odd or even 
-                                        print("even 1") #if it's even when add 1 so it's odd
+                                        #if it's even when add 1 so it's odd
 
                                         pixel[0] = pixel[0]+1
                                         
                                         wordcount = wordcount+1
                                    else : #if it is odd no change is made 
-                                        print("odd 1")
+                                        
                                         wordcount = wordcount+1
                          else : #if it's not time to change a pixel this repeats the cycle
                               redCount = redCount+1
@@ -136,28 +145,28 @@ def encript(img):
                               greenCount = 0
                               blueCount = 1
                               if message[wordcount] == 0 : #if the pixel value will be even
-                                   print("0")
+                                   
                                    if isEven(pixel[1]) : #detects that pixel is even meaning  no changes need to be made 
-                                        print("even 0")
+                                        
                                         #access the red value
                                         wordcount = wordcount+1
                                    else : #detects pixel is odd so it changes  it to even to represent 0 
-                                        print("odd 0")
+                                        
                                         if pixel[1] == 255: # checks to make sure the number is not 255 which is the max value of an RGB value
                                              pixel[1] = pixel[1]-1 #just makes it -1 to make it even instead of odd.    
                                         else:
                                              pixel[1] = pixel[1]+1
                                         wordcount = wordcount+1
                               else : #if the pixel value will be odd
-                                   print("1")
+                                   
                                    if isEven(pixel[1]) :   #checks if the pixel is odd or even 
-                                        print("even 1") #if it's even when add 1 so it's odd
+                                        #if it's even when add 1 so it's odd
 
                                         pixel[1] = pixel[1]+1
                                         
                                         wordcount = wordcount+1
                                    else : #if it is odd no change is made 
-                                        print("odd 1")
+                                        
                                         wordcount = wordcount+1
                          else:
                               greenCount = greenCount+1
@@ -167,40 +176,163 @@ def encript(img):
                               blueCount =0
                               redCount = 1
                               if message[wordcount] == 0 : #if the pixel value will be even
-                                   print("0")
+                                   
                                    if isEven(pixel[2]) : #detects that pixel is even meaning  no changes need to be made 
-                                        print("even 0")
+                                        
                                         #access the red value
                                         wordcount = wordcount+1
                                    else : #detects pixel is odd so it changes  it to even to represent 0 
-                                        print("odd 0")
+                                        
                                         if pixel[2] == 255: # checks to make sure the number is not 255 which is the max value of an RGB value
                                              pixel[2] = pixel[2]-1 #just makes it -1 to make it even instead of odd.    
                                         else:
                                              pixel[2] = pixel[2]+1
                                         wordcount = wordcount+1
                               else : #if the pixel value will be odd
-                                   print("1")
+                                   
                                    if isEven(pixel[2]) :   #checks if the pixel is odd or even 
-                                        print("even 1") #if it's even when add 1 so it's odd
+                                         #if it's even when add 1 so it's odd
 
                                         pixel[2] = pixel[2]+1
                                         
                                         wordcount = wordcount+1
                                    else : #if it is odd no change is made 
-                                        print("odd 1")
+                                        
                                         wordcount = wordcount+1
                          else :
                               blueCount = blueCount+1
         except:
           cv2.imwrite("test.png",finalImg)
+          print("Encription completed")
 
 
 img = get_image(imagepath)
 
 
 
+ # takes the image and interates through it following  the pattern provided to it 
+ #returns a list of  inveded lists where each list is 8 binary chreacter corosponding to an actual Character
+def decript(img):
+     messagelen = pattern["length"]  #get's the information it needs from the key
+     red = pattern["red"]
+     green = pattern["green"]
+     blue = pattern["blue"]
+     message = [] #empty list to append to 
+
+     
+     redCount = 1 #pattern starts with red so it starts with a value above 1 
+     blueCount = 0
+     greenCount = 0
+     characterCount = 0
+
+     
+     for row in img: #row is the actual rows in the image 
+               
+               
+               for pixel in row: #interates by each pixel so the pixel variable is an RGB value set
+                    if characterCount == messagelen: 
+                         break #breaks the loop so I don't repeat the patern until I am out of images
+                    else:
+                         if redCount>0: #if  red is above 0 it's the one  current set to be checked  
+                              if redCount == red : # this detects if it's skipped enough pixels and its time to read one again 
+                                   
+                                   redCount = 0 
+                                   greenCount = 1 #changes green to be the next value to be checked 
+                                   characterCount = characterCount+1 #counts how many chracters have been added
+                                   if isEven(pixel[0]): #we already know  we are lookjing at the right pixel so this just checks if it's even 
+                                        message.append(0) # if it's even it's a 0
+                                   else :
+                                        message.append(1) #otherwise it's a 1 
+                                   
+                              else : 
+                                   redCount = redCount+1 #if it's not the right pixel this continues the countdownm to the right one
+
+
+
+                         elif greenCount>0: #if green is above 0 it's the one  current set to be checked  
+                              if greenCount == green: # this detects if it's skipped enough pixels and its time to read one again 
+                              
+                                   greenCount = 0
+                                   blueCount = 1 #changes blue to be the value being checked 
+                                   characterCount = characterCount+1 #counts how many chracters have been added
+                                   if isEven(pixel[1]): #we already know  we are lookjing at the right pixel so this just checks if it's even 
+                                        message.append(0)# if it's even it's a 0
+                                   else :
+                                        message.append(1)#otherwise it's a 1 
+                                   
+                              else:
+                                   greenCount = greenCount+1 #if it's not the right pixel this continues the countdownm to the right one
+
+
+
+                         elif blueCount>0: #if blue is above 0 it's the one  current set to be checked  
+                              if blueCount == blue: # this detects if it's skipped enough pixels and its time to read one again 
+                              
+                                   blueCount =0
+                                   redCount = 1 #changes red to the value being checked
+                                   characterCount = characterCount+1 #count how many chracters have been added
+                                   if isEven(pixel[2]):  #we already know  we are lookjing at the right pixel so this just checks if it's even 
+                                        message.append(0) # if it's even it's a 0
+                                   else :
+                                        message.append(1) #otherwise it's a 1 
+                                   
+                              else :
+                                   blueCount = blueCount+1 #if it's not the right pixel this continues the countdownm to the right one
+
+     
+     return message 
+
+
+def remakemessage(msg): #receives a embeded list of binary and converts it into a sentence 
+     count = 0
+     character = []
+     total = []
+
+     c = 0
+     for i in msg:
+
+          
+          if count == 7 :
+               character.append(i)
+               total.append(character)
+               count = 0
+               c = c+1
+               character = []
+          else :
+               count = count +1
+               character.append(i)
+
+     
+     final = "" #string that the loop below appends to
+
+
+     for i in total: #loops trhough each list of binary
+          for letter,binary in cypher.items(): #loops through the cypher and checks if the currently selected binary matches
+               if i == binary :
+                    final = final + letter #appends it to the string
+
+     
+     return final 
+
+
+
+
+
+          
+
+
 
 
 encript(img)
+
+
+decriptimg = get_image("test.png")
+
+message = decript(decriptimg)
+
+
+
+print(remakemessage(message))
+
+
 
