1792cd2614874662f428bddddbd5fde6007dfb12
Keegan <keeganxdomso@gmail.com>
2025-04-10 17:06:00 -0600

    Got exe files working

--- Frontend/main.py
+++ Frontend/main.py
@@ -4,16 +4,29 @@ from kivy.lang import Builder
 from kivy.properties import BooleanProperty
 from kivy.uix.button import Button
 from Utils.utils import get_message_bits, get_image, get_message_length, is_even, to_binary, to_char
+import kivy.resources
 import cv2
+import sys
+import os
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
-# Load KV files
-Builder.load_file("Looks/home.kv")
-Builder.load_file("Looks/encrypt.kv")
-Builder.load_file("Looks/decrypt.kv")
+#Fix for PyInstaller packaged paths
+if hasattr(sys, '_MEIPASS'):
+    resource_path = sys._MEIPASS
+else:
+    resource_path = os.path.abspath(".")
+
+# Load KV files using absolute paths
+Builder.load_file(os.path.join(resource_path, "Looks", "home.kv"))
+Builder.load_file(os.path.join(resource_path, "Looks", "encrypt.kv"))
+Builder.load_file(os.path.join(resource_path, "Looks", "decrypt.kv"))
+
+#Add resource path if running from PyInstaller .exe
+if getattr(sys, 'frozen', False):
+    kivy.resources.resource_add_path(sys._MEIPASS)
 
 class HomeScreen(Screen):
     pass
